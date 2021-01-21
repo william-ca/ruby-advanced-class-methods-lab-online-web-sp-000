b13503de2559251c0db22df2837d1d71ee923217
Will Andre <wandre94@gmail.com>
2021-01-21 18:46:03 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -36,6 +36,9 @@ class Song
   def self.find_or_create_by_name(name)
     self.find_by_name(name) || self.create_by_name(name)
   end
-  
+
+  def self.alphabetical
+    @@all.each { |song| song.name <=> song.name }
+  end
 
 end
